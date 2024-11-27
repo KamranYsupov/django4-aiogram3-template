624c74f4e9b0570f3b6363aa1a64b7b65a6f0bdc
Kamran <kamrankek48@gmail.com>
2024-11-27 10:35:55 +0400

    entrypoint.sh encoding change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+п»ї#!/bin/bash
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python manage.py runserver 0:8000 # Использовать только для локальной разработке. Для прода используем gunicorn
+python manage.py runserver 0:8000 # РСЃРїРѕР»СЊР·РѕРІР°С‚СЊ С‚РѕР»СЊРєРѕ РґР»СЏ Р»РѕРєР°Р»СЊРЅРѕР№ СЂР°Р·СЂР°Р±РѕС‚РєРµ. Р”Р»СЏ РїСЂРѕРґР° РёСЃРїРѕР»СЊР·СѓРµРј gunicorn
 
 # gunicorn config.wsgi:application -w 4 --bind 0.0.0.0:8000 --log-level warning
